bf85618b2f32c8cf004e04531a793c89bd0541fd
Milan Gittler <55838375+Cemonix@users.noreply.github.com>
2024-01-24 16:50:58 +0100

    Comment code (#1)

--- src/utility.py
+++ src/utility.py
@@ -16,15 +16,28 @@ def read_signals(path: Path,sampfrom: int = 0, sampto:int | None = None) -> Iter
         sig = rdrecord(header.with_suffix(""),sampfrom=sampfrom,sampto=sampto)
         yield ann, sig
 
-def plot_record(record:Record, generated: npt.NDArray | None = None, height: int=2000, width: int=2000):
+def plot_records(record: Record, generated: npt.NDArray | None = None):
     if record.p_signal is None:
         raise Exception("Need to have loaded signal in record")
     
     figure = make_subplots(rows=record.p_signal.shape[1])
     for row_index, signal in enumerate(record.p_signal.T):
-        figure.add_trace( go.Scatter(y=signal,x=list(range(record.p_signal.shape[0])), showlegend=False, line={"color": "blue"}),row=row_index + 1, col =1)
+        figure.add_trace(
+            go.Scatter(
+                y=signal,x=list(range(record.p_signal.shape[0])),
+                showlegend=False, line={"color": "blue"}
+                ),
+            row=row_index + 1, col =1
+        )
         if generated is not None:
-            figure.add_trace( go.Scatter(y=generated,x=list(range(record.p_signal.shape[0])), showlegend=False, line={"color": "red"}),row=row_index + 1, col =1)
+            figure.add_trace(
+                go.Scatter(
+                    y=generated,x=list(range(record.p_signal.shape[0])),
+                    showlegend=False, line={"color": "red"}
+                ),
+                row=row_index + 1, col =1
+            )
 
     figure.update_layout(height=2000,width=2000)
     return figure
+
